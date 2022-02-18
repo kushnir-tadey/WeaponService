d3e5ef1e05d2f0c9af8259e0d49e16fac70f7956
Tadey <kushnir.tadey@gmail.com>
2022-02-18 15:27:18 +0200

    Changed UUID to Long

--- src/main/java/io/javabrains/weaponservice/repository/WeaponRepository.java
+++ src/main/java/io/javabrains/weaponservice/repository/WeaponRepository.java
@@ -1,11 +1,9 @@
 package io.javabrains.weaponservice.repository;
 
-import java.util.UUID;
-
 import org.springframework.data.cassandra.repository.CassandraRepository;
 
 import io.javabrains.weaponservice.model.Weapon;
 
-public interface WeaponRepository extends CassandraRepository<Weapon, UUID> {
+public interface WeaponRepository extends CassandraRepository<Weapon, Long> {
 
 }
